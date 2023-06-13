1f99a1f09c781b6bc5f98e933529d610f466827c
Tristan Ross <tristan.ross@midstall.com>
2023-06-13 09:05:01 -0700

    fix: typo

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -2,5 +2,5 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/expidus_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
-	expidus_x84_64-user \
-	expidus_x84_64-userdebug
+	expidus_x86_64-user \
+	expidus_x86_64-userdebug
